6c623eead85a1b0dc43b28d706b35c7a738407cd
Jitender Kumar <jkcodingjk@gmail.com>
2023-04-12 18:42:45 +0530

    admin pushed

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+from django.template.loader import get_template
+from django import forms
 
 admin.site.register(Currency)
 admin.site.register(KnowsLanguage)
@@ -44,32 +46,67 @@ class ImageInline(admin.StackedInline):
     model = Image
     extra = 0
 
-class BusinessAdmin(admin.ModelAdmin):
-    list_display = ["type", "map_id", "additional_Type"] 
-    inlines = [AvailableServiceInline,OpeningHoursInline,AdressInline,GeoInline,AreaServedInline,ContactPointInline,EmployeeInline,ImageInline]
-
-admin.site.register(Business, BusinessAdmin)
-
-
 class HaspartInline(admin.StackedInline):
     model = Haspart
     extra = 0
 
-class WebsiteAdmin(admin.ModelAdmin):
-    inlines = [HaspartInline,]
+class BusinessAdmin(admin.ModelAdmin):
+    fields = ["type",'map_id','additional_Type','Avail','medical_Specialty','curreny_Accepted','Open','payment_Accepted','Adress','Geo','hasMap','logo','telephone','isAcceptingNewPatients','AreaServed','ContactPoint','Employee','knows_language','Image','url','name','AlternateName','description','disambiguatingDescription','foundingDate','SameAs','pub_type','publisher_id','pub_name','website_id','pub_url','Has',]
+  
 
-admin.site.register(Website, WebsiteAdmin)
+    inlines = [AvailableServiceInline,OpeningHoursInline,AdressInline,GeoInline,AreaServedInline,ContactPointInline,EmployeeInline,ImageInline,HaspartInline]
+    readonly_fields = ('Avail','Open','Adress','Geo','AreaServed','ContactPoint','Employee','Image','Has',) 
 
+    def Avail(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def Open(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def Adress(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def Geo(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def AreaServed(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def ContactPoint(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def Employee(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def Image(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
+    
+    def Has(self, *args, **kwargs):
+        context = getattr(self.response, 'context_data', None) or {}
+        inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        return get_template(inline.opts.template).render(context, self.request)
 
+    def render_change_form(self, request, *args, **kwargs):
+        self.request = request
+        self.response = super().render_change_form(request, *args, **kwargs)
+        return self.response
+    
+    
 
-
-
-
-
-
-
-
-
-
-
-
+admin.site.register(Business, BusinessAdmin)
